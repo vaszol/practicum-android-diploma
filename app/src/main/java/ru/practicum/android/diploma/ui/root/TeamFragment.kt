2e3ecefb25d09babca57e652da99f2716b633bbc
Pavel_Davydov <david20506@yandex.ru>
2024-11-08 19:04:39 +0500

    Добавил пустых фрагментов. BottomNavigation работает

---
+++ app/src/main/java/ru/practicum/android/diploma/ui/root/TeamFragment.kt
@@ -0,0 +1,19 @@
+package ru.practicum.android.diploma.ui.root
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import ru.practicum.android.diploma.R
+
+class TeamFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_team, container, false)
+    }
+
+}
