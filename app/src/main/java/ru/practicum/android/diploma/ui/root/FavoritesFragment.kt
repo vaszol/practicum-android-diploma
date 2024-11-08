7d222a90c56e72943f441970f2a1fed413c0edde
Pavel_Davydov <david20506@yandex.ru>
2024-11-08 19:21:18 +0500

    Пытаюсь ублажить reviewdog

--- app/src/main/java/ru/practicum/android/diploma/ui/root/FavoritesFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/root/FavoritesFragment.kt
@@ -10,7 +10,8 @@ import ru.practicum.android.diploma.R
 class FavoritesFragment : Fragment() {
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_favorites, container, false)
--- app/src/main/java/ru/practicum/android/diploma/ui/root/MainFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/root/MainFragment.kt
@@ -10,7 +10,8 @@ import ru.practicum.android.diploma.R
 class MainFragment : Fragment() {
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_main, container, false)
