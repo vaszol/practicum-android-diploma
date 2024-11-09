0353b89bd7afe5ed34cd09d6e3fe819ed96d90ab
Basalygina <korneva074@gmail.com>
2024-11-09 21:23:21 +0100

    исправлены замечания reviewdog

--- app/src/main/java/ru/practicum/android/diploma/data/dto/AreaDto.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/dto/AreaDto.kt
@@ -2,7 +2,7 @@ package ru.practicum.android.diploma.data.dto
 
 data class AreaDto(
     val id: String,
-    val parentId: String? = null, //Идентификатор родительского региона
+    val parentId: String? = null, // Идентификатор родительского региона
     val name: String,
-    val areas: List<AreaDto> = emptyList()  //Список вложенных областей
+    val areas: List<AreaDto> = emptyList() // Список вложенных областей
 )
