6da9b36313dcbaa173de602076b279f368d26fa9
Basalygina <korneva074@gmail.com>
2024-11-10 10:38:27 +0100

    Выделен отдельный класс BaseDTO

--- app/src/main/java/ru/practicum/android/diploma/data/dto/AreaDto.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/dto/AreaDto.kt
@@ -2,7 +2,7 @@ package ru.practicum.android.diploma.data.dto
 
 data class AreaDto(
     val id: String,
-    val parentId: String? = null, // Идентификатор родительского региона
     val name: String,
+    val parentId: String? = null, // Идентификатор родительского региона
     val areas: List<AreaDto> = emptyList() // Список вложенных областей
 )
---
+++ app/src/main/java/ru/practicum/android/diploma/data/dto/BaseDto.kt
@@ -0,0 +1,3 @@
+package ru.practicum.android.diploma.data.dto
+
+data class BaseDto(val id: String, val name: String?)
