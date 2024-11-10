c5c4df1688afaf75b544724c6d3ab629cea9729c
Basalygina <korneva074@gmail.com>
2024-11-10 12:02:43 +0100

    Подготовлены доменные модели Vacancy и Area Созданы функции для конвертации из DTO в Domain-модели

--- app/src/main/java/ru/practicum/android/diploma/data/dto/VacancyDto.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/dto/VacancyDto.kt
@@ -1,5 +1,8 @@
 package ru.practicum.android.diploma.data.dto
 
+import ru.practicum.android.diploma.domain.models.Area
+import ru.practicum.android.diploma.domain.models.Vacancy
+
 data class VacancyDto(
     val id: String,
     val name: String?,
@@ -14,3 +17,25 @@ data class VacancyDto(
     val currency: String?, // Код валюты, salary.currency
     val description: String?, // Описание вакансии в формате HTML
 )
+
+fun VacancyDto.toDomain(): Vacancy =
+    Vacancy(
+        id = id,
+        name = name ?: Vacancy.VACANCY_DEFAULT_STRING_VALUE,
+        employerName = employerName ?: Vacancy.VACANCY_DEFAULT_STRING_VALUE,
+        employerLogoUrl90 = employerLogoUrl90 ?: Vacancy.VACANCY_DEFAULT_STRING_VALUE,
+        area = area?.toDomain() ?: Area(
+            id = Vacancy.VACANCY_DEFAULT_STRING_VALUE,
+            name = Vacancy.VACANCY_DEFAULT_STRING_VALUE,
+            parentId = Vacancy.VACANCY_DEFAULT_STRING_VALUE,
+            areas = emptyList()
+        ),
+        experience = experience?.name ?: Vacancy.VACANCY_DEFAULT_STRING_VALUE,
+        employment = employment?.name ?: Vacancy.VACANCY_DEFAULT_STRING_VALUE,
+        schedule = schedule?.name ?: Vacancy.VACANCY_DEFAULT_STRING_VALUE,
+        salaryFrom = salaryFrom ?: Vacancy.VACANCY_DEFAULT_INT_VALUE,
+        salaryTo = salaryTo ?: Vacancy.VACANCY_DEFAULT_INT_VALUE,
+        currency = currency ?: Vacancy.CURRENCY_DEFAULT_VALUE,
+        description = description ?: Vacancy.VACANCY_DEFAULT_STRING_VALUE
+    )
+
---
+++ app/src/main/java/ru/practicum/android/diploma/domain/models/Vacancy.kt
@@ -0,0 +1,35 @@
+package ru.practicum.android.diploma.domain.models
+
+import java.io.Serializable
+
+data class Vacancy(
+    val id: String,
+    val name: String,
+    val employerName: String,
+    val employerLogoUrl90: String, // URL логотипа (90x90)
+    val area: Area, // Регион
+    val experience: String, // Опыт работы
+    val employment: String, // Тип занятости
+    val schedule: String, // График работы
+    val salaryFrom: Int, // Нижний предел зарплаты
+    val salaryTo: Int, // Верхний предел зарплаты
+    val currency: String, // Код валюты
+    val description: String // Описание вакансии в формате HTML
+) : Serializable {
+    override fun hashCode(): Int {
+        return id.hashCode()
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+        if (id != (other as Vacancy).id) return false
+        return true
+    }
+
+    companion object {
+        const val VACANCY_DEFAULT_STRING_VALUE = "-1"
+        const val VACANCY_DEFAULT_INT_VALUE = -1
+        const val CURRENCY_DEFAULT_VALUE = "RUR"
+    }
+}
