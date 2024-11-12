7b0876f69b48324bc4452dfaf19fad0f0eff6265
Mikle-04 <godmordor@gmail.com>
2024-11-12 17:21:02 +0300

    layout_search_correct_team

---
+++ app/src/main/java/ru/practicum/android/diploma/ui/root/details/DetailsFragment.kt
@@ -0,0 +1,26 @@
+package ru.practicum.android.diploma.ui.root.details
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import ru.practicum.android.diploma.R
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [DetailsFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class DetailsFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_details, container, false)
+    }
+
+}
--- app/src/main/java/ru/practicum/android/diploma/ui/root/search/MainFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/root/search/MainFragment.kt
@@ -1,4 +1,4 @@
-package ru.practicum.android.diploma.ui.root
+package ru.practicum.android.diploma.ui.root.search
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,14 +7,13 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import ru.practicum.android.diploma.R
 
-class TeamFragment : Fragment() {
+class MainFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_team, container, false)
+        return inflater.inflate(R.layout.fragment_main, container, false)
     }
-
 }
