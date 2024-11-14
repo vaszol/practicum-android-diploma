a354dd17dd3eef4d46eb137d02cca41b1529be8a
Pavel_Davydov <david20506@yandex.ru>
2024-11-14 15:21:51 +0500

    Экран 11

---
+++ app/src/main/java/ru/practicum/android/diploma/ui/root/place/SelectPlaceFragment.kt
@@ -0,0 +1,16 @@
+package ru.practicum.android.diploma.ui.root.place
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import ru.practicum.android.diploma.R
+
+class SelectPlaceFragment: Fragment() {
+    override fun onCreateView(inflater: LayoutInflater,
+                              container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return return inflater.inflate(R.layout.fragment_select_place, container, false)
+    }
+}
