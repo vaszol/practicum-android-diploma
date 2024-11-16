f8fa7d6091bfd3b6000339dc222137216708e450
Basalygina <korneva074@gmail.com>
2024-11-16 09:00:59 +0100

    Исправлена ошибка с отображением логотипов на экране поиска

--- app/src/main/java/ru/practicum/android/diploma/ui/root/search/VacancyViewHolder.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/root/search/VacancyViewHolder.kt
@@ -3,6 +3,7 @@ package ru.practicum.android.diploma.ui.root.search
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.databinding.ItemVacancyBinding
 import ru.practicum.android.diploma.domain.models.Vacancy
@@ -14,13 +15,14 @@ class VacancyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
     fun bind(model: Vacancy) {
         with(binding) {
-            vacancyTitle.text = "${model.name}, ${model.area?.name}"
+            vacancyTitle.text = "${model.name}, ${model.area.name}"
             employerName.text = model.employerName
             salary.text = model.formatSalary()
             if (model.employerLogoUrl90 != VACANCY_DEFAULT_STRING_VALUE) {
                 Glide.with(companyLogo.context)
                     .load(model.employerLogoUrl90)
                     .placeholder(R.drawable.employer_logo_placeholder)
+                    .transform(RoundedCorners(12))
                     .into(companyLogo)
             } else {
                 companyLogo.setImageResource(R.drawable.employer_logo_placeholder)
