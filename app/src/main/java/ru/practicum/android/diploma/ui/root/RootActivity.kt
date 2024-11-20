5b0f31c566e031bda55a7ad48ef16ed85a991a0b
Pavel_Davydov <david20506@yandex.ru>
2024-11-21 02:52:38 +0500

    Принудительная установка ресурсов в "ru" локаль

--- app/src/main/java/ru/practicum/android/diploma/ui/root/RootActivity.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/root/RootActivity.kt
@@ -1,19 +1,28 @@
 package ru.practicum.android.diploma.ui.root
 
 import android.annotation.SuppressLint
+import android.content.Context
 import android.content.pm.ActivityInfo
+import android.content.res.Configuration
+import android.content.res.Resources
+import android.os.Build
 import android.os.Bundle
+import android.util.DisplayMetrics
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.databinding.ActivityRootBinding
+import java.util.Locale
 
 class RootActivity : AppCompatActivity() {
 
     @SuppressLint("SourceLockedOrientationActivity")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setAppLocaleRu(this)
+
         requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
 
         val binding = ActivityRootBinding.inflate(layoutInflater)
@@ -23,5 +32,17 @@ class RootActivity : AppCompatActivity() {
         val navController = navHostFragment.navController
 
         binding.bottomNavigationView.setupWithNavController(navController)
+    }
+
+    private fun setAppLocaleRu(context: Context) {
+        val resources: Resources = context.resources
+        val dm: DisplayMetrics = resources.displayMetrics
+        val config: Configuration = resources.configuration
+
+        val locale = Locale("ru")
+
+        config.setLocale(locale)
+
+        resources.updateConfiguration(config, dm)
     }
 }
